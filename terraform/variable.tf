e6b1645a32b535658bfb2d6100de3bcad9b000b1
0712Anand <1032210643@tcetmumbai.in>
2025-03-19 20:27:07 +0530

    third

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,18 +1,18 @@
 variable "s3_bucket"{
     description = "Bucket name for s3"
     type  = string
-    default = "anand-reddit-bucket"
+    default = "xx"
 
 }
 variable "db_password"{
     description = "Redshift password for master DB"
     type  = string
-    default = "Anand2003"
+    default = "xx"
 
 }
 variable "aws_region" {
   description = "Region for AWS"
   type        = string
-  default     = "ap-south-1"
+  default     = "x"
 }
 
